f1e38fa362dd970dc4ae4a330ff1e2979192c52c
marc <marc.haefeker@student.uni-tuebingen.de>
2018-06-13 17:51:54 +0200

    Version 0.1.2
    Adding Homepage-Contents.....

--- ressources/startseite_inhalte.php
+++ ressources/startseite_inhalte.php
@@ -9,7 +9,14 @@
 function startseite_inhalt_home(){
 
     #Parallax 1
-    $Section1Content = '';
+    $Section1Content = '<br><br><h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        </div>
+        <div class="row center">
+          <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+        </div>
+        <br><br>';
     $Section1ContentContainer = container_builder($Section1Content);
     $Section1 = section_builder($Section1ContentContainer, '', 'no-pad-bot');
     $Parallax1Content = '';
