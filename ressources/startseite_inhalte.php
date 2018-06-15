821c0808ff2dcd035c73cface9ffb2446fbcc1dd
marc <marc.haefeker@student.uni-tuebingen.de>
2018-06-15 14:37:47 +0200

    Version 0.2.3
    Adding Homepage-Contents..... started implementing frontend-cms..
    
    adding htmlenteties() .......

--- ressources/startseite_inhalte.php
+++ ressources/startseite_inhalte.php
@@ -64,10 +64,10 @@ function parallax_mit_text_generieren($BausteinID){
         $Ergebnis = mysqli_fetch_assoc($Abfrage);
 
         # Content generieren
-        $Ueberschrift = '<br><br><h1 class="header center teal-text text-lighten-2">' . htmlentities($Ergebnis['ueberschrift'],'', "UTF-8") . '</h1>';
+        $Ueberschrift = '<br><br><h1 class="header center teal-text text-lighten-2">' . htmlentities($Ergebnis['ueberschrift'],ENT_QUOTES | ENT_IGNORE, "UTF-8") . '</h1>';
 
         if ($Ergebnis['zweite_ueberschrift'] != '') {
-            $Ueberschrift2 = '<div class="row center"><h5 class="header col s12 light">' . htmlentities($Ergebnis['zweite_ueberschrift'], '', "UTF-8") . '</h5></div>';
+            $Ueberschrift2 = '<div class="row center"><h5 class="header col s12 light">' . htmlentities($Ergebnis['zweite_ueberschrift'], ENT_QUOTES | ENT_IGNORE, "UTF-8") . '</h5></div>';
         } else {
             $Ueberschrift2 = '';
         }
