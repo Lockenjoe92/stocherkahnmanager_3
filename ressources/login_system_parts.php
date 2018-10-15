3c42bc8afdc307016db619a60fe13d2d44635d99
marc <marc.haefeker@student.uni-tuebingen.de>
2018-10-15 18:42:23 +0200

    Version 0.2.6
    Started creating login-system:
    
    - added form elements

--- ressources/login_system_parts.php
+++ ressources/login_system_parts.php
@@ -2,18 +2,20 @@
 
 function login_formular(){
 
-    $HTML = "<div class='row'>";
+    $HTML = "<div class='row center'>";
     $HTML .= "<form action='#' method='post' class='col s12'>";
 
     $HTML .= "<div class='row'>";
     $HTML .= "<div class='input-field col s6'>";
-    $HTML .= "<input type='email' name='mail'>";
+    $HTML .= "<input id='login_mail' type='email' name='mail'>";
+    $HTML .= "<label for='login_mail'>Mail</label>";
     $HTML .= "</div>";
     $HTML .= "</div>";
 
     $HTML .= "<div class='row'>";
     $HTML .= "<div class='input-field col s6'>";
-    $HTML .= "<input type='password' name='pass'>";
+    $HTML .= "<input id='login_pswd' type='password' name='pass'>";
+    $HTML .= "<label for='login_pswd'>Passwort</label>";
     $HTML .= "</div>";
     $HTML .= "</div>";
 
