21832a0bd8a708c96fe1b7a132a69a3f7d018011
marc <marc.haefeker@student.uni-tuebingen.de>
2018-10-15 18:10:06 +0200

    Version 0.2.6
    Started creating login-system:
    
    - created login.php

--- login.php
+++ login.php
@@ -2,19 +2,20 @@
 /**
  * Created by PhpStorm.
  * User: marc
- * Date: 12.06.18
- * Time: 18:13
+ * Date: 15.10.18
+ * Time: 18:04
  */
 
 # Include all ressources
 include_once "./ressources/ressourcen.php";
 
-# Generate Content
-$HTML = startseite_inhalt_home();
-$Header = "Home - " . lade_xml_einstellung('site_name', 'local');
+#Generate Content
+$Header = "Login - " . lade_xml_einstellung('site_name', 'local');
+$HTML = "Test";
 
 # Output site
 echo site_header($Header);
 echo site_body($HTML);
+echo site_footer();
 
 ?>
