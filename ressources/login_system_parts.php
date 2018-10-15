4a5a88da8055a612bf6e7b4bb1a7c7b007dfe44b
marc <marc.haefeker@student.uni-tuebingen.de>
2018-10-15 18:40:02 +0200

    Version 0.2.6
    Started creating login-system:
    
    - added form elements

--- ressources/login_system_parts.php
+++ ressources/login_system_parts.php
@@ -2,14 +2,28 @@
 
 function login_formular(){
 
-    $HTML = "<div class='row center'>";
-    $HTML .= "<div class='container'>";
-    $HTML .= "<form action='#' method='post'>";
+    $HTML = "<div class='row'>";
+    $HTML .= "<form action='#' method='post' class='col s12'>";
+
+    $HTML .= "<div class='row'>";
+    $HTML .= "<div class='input-field col s6'>";
     $HTML .= "<input type='email' name='mail'>";
-    $HTML .= "<input type='password' name='pass'>";
-    $HTML .= "<input type='submit' name='submit'>";
-    $HTML .= "</form>";
     $HTML .= "</div>";
+    $HTML .= "</div>";
+
+    $HTML .= "<div class='row'>";
+    $HTML .= "<div class='input-field col s6'>";
+    $HTML .= "<input type='password' name='pass'>";
+    $HTML .= "</div>";
+    $HTML .= "</div>";
+
+    $HTML .= "<div class='row'>";
+    $HTML .= "<div class='input-field col s6'>";
+    $HTML .= "<input type='submit' name='submit'>";
+    $HTML .= "</div>";
+    $HTML .= "</div>";
+
+    $HTML .= "</form>";
     $HTML .= "</div>";
 
     return $HTML;
