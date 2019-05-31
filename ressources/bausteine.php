2727f370828d6b281a380e0cb5cdc8cf09bfca61
marc <marc.haefeker@student.uni-tuebingen.de>
2019-05-31 19:46:26 +0200

    Start adding Settings Functionality

--- ressources/bausteine.php
+++ ressources/bausteine.php
@@ -80,7 +80,7 @@ function form_builder($ContentHTML, $ActionPageLink, $ID=''){
 function form_switch_item($ItemName, $OptionLeft='off', $OptionRight='on', $BooleanText='false', $Disabled=false){
 
     $HTML = "<div class='switch'>";
-    $HTML .= "".$ItemName." ";
+    $HTML .= "<h4>".$ItemName."</h4>   ";
     $HTML .= "<label>";
     $HTML .= $OptionLeft;
 
