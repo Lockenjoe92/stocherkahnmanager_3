40aff41e4161d043c72c934121528e05d83c6433
marc <marc.haefeker@student.uni-tuebingen.de>
2019-05-31 20:11:32 +0200

    Start adding Settings Functionality

--- admin_settings.php
+++ admin_settings.php
@@ -9,16 +9,8 @@ $Header = "Admin Einstellungen - " . lade_xml_einstellung('site_name', 'local');
 $PageTitle = '<h1>Admineinstellungen</h1>';
 $HTML = container_builder($PageTitle, 'admin_settings_page_title');
 
-# Settings List
-$FormTableItems = table_form_swich_item('Seitenaktivierung', 'Deaktiviert', 'Aktiviert', 'false', true);
-$FormTable = table_builder($FormTableItems);
-$FormHTML = form_builder($FormTable, './admin_settings.php', 'admin_settings_form');
-
-$HTML .= container_builder($FormHTML, 'admin_settings_form_section');
-$ContainerHTML = container_builder($HTML);
-
 # Output site
 echo site_header($Header);
-echo site_body($ContainerHTML);
+echo site_body($HTML);
 
 ?>
