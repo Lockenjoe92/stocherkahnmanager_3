5feed95de7fd244b83a1ab0d66c7934193f72399
marc <marc.haefeker@student.uni-tuebingen.de>
2019-05-31 19:04:42 +0200

    Start adding Settings Functionality

--- admin_settings.php
+++ admin_settings.php
@@ -2,15 +2,15 @@
 
 include_once "./ressources/ressourcen.php";
 session_manager('ist_admin');
+$Header = "Admin Einstellungen - " . lade_xml_einstellung('site_name', 'local');
 
+#Generate content
 $HTML = '<h1>Hello World!</h1>';
-
-#Create Page Sections
 $HTML = section_builder($HTML, 'admin_settings_page_title');
-
-#Put it all into a container
 $HTML = container_builder($HTML, 'admin_settings_main_container');
 
-echo $HTML;
+# Output site
+echo site_header($Header);
+echo site_body($HTML);
 
 ?>
