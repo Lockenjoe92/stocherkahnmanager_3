f9052472f60f4fa2646bbe30d001052d63bf8677
marc <marc.haefeker@student.uni-tuebingen.de>
2019-06-03 14:01:49 +0200

    Start adding Logout

--- administration.php
+++ administration.php
@@ -6,6 +6,17 @@
  * Time: 13:59
  */
 
-echo "Adminpage";
+include_once "./ressources/ressourcen.php";
+session_manager('ist_admin');
+$Header = "Adminpage - " . lade_db_einstellung('site_name');
+
+#Generate content
+# Page Title
+$PageTitle = '<h1>Adminseite</h1>';
+$HTML .= section_builder($PageTitle);
+
+# Output site
+echo site_header($Header);
+echo site_body($HTML);
 
 ?>
