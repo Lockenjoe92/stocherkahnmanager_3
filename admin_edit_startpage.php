2e748c1154ef3bb89563ad256851f840948d8927
marc <marc.haefeker@student.uni-tuebingen.de>
2019-06-03 15:13:22 +0200

    Start adding Logout

---
+++ admin_edit_startpage.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marc
+ * Date: 03.06.19
+ * Time: 13:59
+ */
+
+include_once "./ressources/ressourcen.php";
+session_manager('ist_admin');
+$Header = "Startseite Editieren - " . lade_db_einstellung('site_name');
+
+#Generate content
+# Page Title
+$PageTitle = '<h1>Startseite Editieren</h1>';
+$HTML .= section_builder($PageTitle);
+
+
+# Output site
+echo site_header($Header);
+echo site_body($HTML);
+
+?>
