5869bf0491c0ae66a055d96d6560e91b856c7ec2
marc <marc.haefeker@student.uni-tuebingen.de>
2019-06-04 11:04:54 +0200

    Version 0.4.0
    fixing toasts

--- admin_edit_startpage.php
+++ admin_edit_startpage.php
@@ -8,10 +8,9 @@
 
 include_once "./ressources/ressourcen.php";
 session_manager('ist_admin');
+echo 'huhu';
 $link = connect_db();
 $Header = "Webseite Editieren - " . lade_db_einstellung('site_name');
-
-echo 'huhu';
 
 #Generate content
 # Page Title
