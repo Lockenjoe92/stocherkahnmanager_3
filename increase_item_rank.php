874a8c276e84dcb0b7e808eb997ae0f0e0c6307b
marc <marc.haefeker@student.uni-tuebingen.de>
2019-06-06 12:43:52 +0200

    Version 0.4.0
    fixing toasts

---
+++ increase_item_rank.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marc
+ * Date: 06.06.19
+ * Time: 12:43
+ */
+
+include_once "./ressources/ressourcen.php";
+session_manager('ist_admin');
+$link = connect_db();
+$Baustein = $_GET['baustein'];
+$Item = $_GET['item'];
+
+if((intval($Baustein)>0) and (intval($Item)>0)){
+
+    $ItemMeta = lade_seiteninhalt($Item);
+    $ItemRang = $ItemMeta['rang'];
+
+    #Calculate new Rang
+    $NewRang = $ItemRang + 1;
+
+    #Load the other item
+    $Anfrage = "SELECT * FROM homepage_content WHERE id_baustein = ".$Baustein." AND rang = ".$NewRang." AND storno_user = 0";
+    $Abfrage = mysqli_query($link, $Anfrage);
+    $Ergebnis = mysqli_fetch_assoc($Abfrage);
+
+    # Update selected Item
+    update_website_content_item($Item, 'rang', $NewRang);
+
+    # Update corresponding Item
+    update_website_content_item($Ergebnis['id'], 'rang', $NewRang);
+
+    header("Location: ./admin_edit_startpage.php");
+    die();
+} else {
+    header("Location: ./admin_edit_startpage.php");
+    die();
+}
