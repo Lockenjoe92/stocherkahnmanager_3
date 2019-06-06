f6c04e1de9ef156b2b1aaaf1b2c120489753d0ee
marc <marc.haefeker@student.uni-tuebingen.de>
2019-06-06 12:16:36 +0200

    Version 0.4.0
    fixing toasts

---
+++ add_website_item.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marc
+ * Date: 06.06.19
+ * Time: 12:14
+ */
+
+include_once "./ressources/ressourcen.php";
+session_manager('ist_admin');
+$Baustein = $_GET['baustein'];
+
+if(intval($Baustein)>0){
+    startseiteninhalt_einfuegen($Baustein, 'Neues Element', '', '', '', 'Hier entsteht ein neues Element', '', 'announcement', '');
+    header("Location: ./admin_edit_startpage.php");
+    die();
+} else {
+    header("Location: ./admin_edit_startpage.php");
+    die();
+}
