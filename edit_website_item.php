efe249afc578a95b77508c66c84618491aa735e4
marc <marc.haefeker@student.uni-tuebingen.de>
2019-06-06 11:43:57 +0200

    Version 0.4.0
    fixing toasts

--- edit_website_item.php
+++ edit_website_item.php
@@ -28,6 +28,8 @@ if(intval($Item)>0){
     $HTML .= section_builder('<h5>Inhaltselement</h5>');
     if ($BausteinMeta['typ'] == 'row_container'){
         $HTML .= generate_row_item_change_form($Item);
+    } elseif ($BausteinMeta['typ'] == 'parallax_mit_text'){
+        $HTML .= generate_parallax_change_form($Item);
     }
 
     # Output site
@@ -78,6 +80,27 @@ function generate_row_item_change_form($Item){
 
 }
 
+function generate_parallax_change_form($Item){
+
+    $ItemMeta = lade_seiteninhalt($Item);
+
+    $TableRows = table_form_string_item('Überschrift', 'item_title', $ItemMeta['ueberschrift'], '');
+    $TableRows .= table_form_string_item('Überschriftfarbe', 'item_title_color', $ItemMeta['ueberschrift_farbe'], '');
+    $TableRows .= table_form_string_item('Zweite Überschrift', 'second_item_title', $ItemMeta['zweite_ueberschrift'], '');
+    $TableRows .= table_form_string_item('Zweite Überschriftfarbe', 'second_item_title_color', $ItemMeta['zweite_ueberschrift_farbe'], '');
+    $TableRows .= table_form_html_area_item('Inhalt HTML', 'item_html', $ItemMeta['html_content'], '');
+    $TableRows .= table_form_string_item('URI Bild', 'item_pic_uri', $ItemMeta['uri_bild'], '');
+
+    $TableRowContent = table_data_builder(button_link_creator('Zurück', './admin_edit_startpage.php', 'arrow_back', ''));
+    $TableRowContent .= table_header_builder(form_button_builder('action_edit_site_item', 'Bearbeiten', 'action', 'edit', ''));
+    $TableRows .= table_row_builder($TableRowContent);
+    $Table = table_builder($TableRows);
+    $Form = form_builder($Table, '#', 'post', 'item_change_form');
+    $Section = section_builder($Form);
+
+    return $Section;
+}
+
 function parse_edit_website_item_page($Item){
 
     if (isset($_POST['action_edit_site_item'])){
@@ -86,6 +109,8 @@ function parse_edit_website_item_page($Item){
 
         if ($BausteinMeta['typ'] == 'row_container'){
             parse_row_item_edit($Item);
+        } elseif ($BausteinMeta['typ'] == 'parallax_mit_text'){
+            parse_parallax_item_edit($Item);
         }
     }
 }
@@ -102,6 +127,25 @@ function parse_row_item_edit($Item){
     update_website_content_item($Item, 'ueberschrift', $_POST['item_title']);
     update_website_content_item($Item, 'html_content', $HTMLValue);
     update_website_content_item($Item, 'icon', $_POST['item_icon']);
+
+
+}
+
+function parse_parallax_item_edit($Item){
+
+    #Remove certain HTML Tags from HTML-Textarea-Input
+    $HTMLValue = $_POST['item_html'];
+    $HTMLValue = str_replace('<pre>','',$HTMLValue);
+    $HTMLValue = str_replace('<code>','',$HTMLValue);
+    $HTMLValue = str_replace('</code>','',$HTMLValue);
+    $HTMLValue = str_replace('</pre>','',$HTMLValue);
+
+    update_website_content_item($Item, 'ueberschrift', $_POST['item_title']);
+    update_website_content_item($Item, 'ueberschrift_farbe', $_POST['item_title_color']);
+    update_website_content_item($Item, 'zweite_ueberschrift', $_POST['second_item_title']);
+    update_website_content_item($Item, 'zweite_ueberschrift_farbe', $_POST['second_item_title_color']);
+    update_website_content_item($Item, 'html_content', $HTMLValue);
+    update_website_content_item($Item, 'uri_bild', $_POST['item_pic_uri']);
 
 
 }
